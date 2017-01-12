626af4da8ced1d898c04b0b6e257625eaafec7c5
wya <1052700441@qq.com>
2017-01-12 20:19:11 +0800

    bushu

--- p/p/wsgi.py
+++ p/p/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))#3
+import sys # 4
+sys.path.insert(0,PROJECT_DIR) # 5
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "p.settings")
 
 application = get_wsgi_application()
